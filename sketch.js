5571fe9a1aadcc735c548975eb962a69c25a8f34
Julian Abhari <dinoboy8@gmail.com>
2025-01-22 21:04:05 -0600

    Added svg export for laser cutting

--- sketch.js
+++ sketch.js
@@ -1,29 +1,36 @@
 // First function that gets executed
 function setup() {
-  createCanvas(1500, 800);
+  createCanvas(800, 800, SVG);
   pixelDensity(3);
 }
 
 // Function that gets called every frame.
 function draw() {
   // Replacing the background of the canvas with this color
-  background(0);
+  background(255);
 
   var x = width / 2;
   var y = height / 2;
 
-  for (var i = 1; i < TWO_PI * 2; i += (12 * PI) / 180) {
-    for (var j = 0; j < TWO_PI; j += PI / 180) {
-      var color_index = map(j, 0, TWO_PI, 0, 255);
-      noStroke();
+  for (var i = 1; i < TWO_PI; i += (8 * PI) / 180) {
+    for (var j = 0; j < TWO_PI * 2; j += PI / 180) {
+      // var color_index = map(j, 0, TWO_PI * 2, 0, 255);
+      fill(255, 0, 0);
+      stroke(255, 0, 0);
+      strokeWeight(2);
       arc(
-        x + ((0.25 * (j * 180)) / PI) * i * cos(i),
-        y + ((0.25 * (j * 180)) / PI) * i * sin(i) + 100,
-        (0.25 * (j * 180)) / PI,
-        (0.25 * (j * 180)) / PI,
+        x + ((0.08 * (j * 180)) / PI) * i * cos(i),
+        y + ((0.08 * (j * 180)) / PI) * i * sin(i),
+        (0.08 * (j * 180)) / PI,
+        (0.08 * (j * 180)) / PI,
         j,
         j + PI / 180,
       );
     }
   }
+}
+
+function mousePressed() {
+  console.log("Downloading Canvas as SVG!");
+  save("sketch.svg");
 }
